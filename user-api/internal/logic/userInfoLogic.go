8d07aa2281796ebd12733e30078a5ffac913dd66
gaoh <gaojiancheng@shwpg.com>
2022-09-19 09:39:07 +0800

    user-api

---
+++ user-api/internal/logic/userInfoLogic.go
@@ -0,0 +1,32 @@
+package logic
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GitSorcerer/go-zero-stu/user-api/internal/svc"
+	"github.com/GitSorcerer/go-zero-stu/user-api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserInfoLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserInfoLogic {
+	fmt.Println("Creating UserInfoLogic...")
+	return &UserInfoLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserInfoLogic) UserInfo(req *types.UserInfoReq) (resp *types.UserInfoResp, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
